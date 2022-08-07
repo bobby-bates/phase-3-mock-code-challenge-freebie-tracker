678f75658d30ca3c2bccce72fc17b4d91c21576a
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-07 17:39:55 -0400

    complete Freebie object association methods

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,4 +1,14 @@
 class Freebie < ActiveRecord::Base
   belongs_to :dev
   belongs_to :company
+
+  # Naming this method #dev made self.dev recursively call itself instead of
+  # returning the Dev instance, so it's called dev_instance now
+  def dev_instance
+    self.dev
+  end
+
+  def company_instance
+    self.company
+  end
 end
