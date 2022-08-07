c43c55759e7c24def5eba4e5ef4c611bf21c18a0
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-07 17:11:50 -0400

    build dev, company, and freebie migrations, and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,15 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(item_name: 'Glass', value: 999, dev_id: 1, company_id: 1)
+Freebie.create(item_name: 'Poker', value: 20, dev_id: 2, company_id: 2)
+Freebie.create(item_name: 'Stapler', value: 12, dev_id: 3, company_id: 3)
+Freebie.create(item_name: 'Insider Trading Information', value: 999999999, dev_id: 4, company_id: 4)
 
 puts "Seeding done!"
