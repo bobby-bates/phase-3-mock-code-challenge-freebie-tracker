a73b50de8cad47461c0052594cbfc58e458e8f53
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-07 17:47:42 -0400

    complete Company object association methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,12 @@
 class Company < ActiveRecord::Base
   has_many :freebies
-  # has_many :devs, through: :freebies
+  has_many :devs, through: :freebies
+
+  def freebies_collection
+    self.freebies
+  end
+
+  def devs_collection
+    self.devs
+  end
 end
